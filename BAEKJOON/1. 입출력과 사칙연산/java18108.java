bbee9b4523fa70c694632ef6fd83d900cd1d2d58
9antry <9antry@gmail.com>
2022-10-07 00:13:34 +0900

    java 1007

---
+++ BAEKJOON/1. 입출력과 사칙연산/java18108.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class java18108 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int year1 = sc.nextInt();
+        System.out.println(year1 - 543);
+        sc.close();
+    }
+}
