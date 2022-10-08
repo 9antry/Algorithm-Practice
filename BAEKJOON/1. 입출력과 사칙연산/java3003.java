47bc818f51b5e190e8974fe4b3f7bcc8217a4034
9antry <9antry@gmail.com>
2022-10-08 14:26:38 +0900

    java3003 1008

---
+++ BAEKJOON/1. 입출력과 사칙연산/java3003.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class java3003 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int K = sc.nextInt();
+        int Q = sc.nextInt();
+        int L = sc.nextInt();
+        int B = sc.nextInt();
+        int N = sc.nextInt();
+        int P = sc.nextInt();
+
+        System.out.println((1-K) +" "+ (1-Q) +" "+ (2-L) +" "+ (2-B) +" "+ (2-N) +" "+ (8-P));
+    }
+}
