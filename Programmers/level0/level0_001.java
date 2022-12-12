d389ddf019a5aa1ce96aadb63fef13cab9a383bd
9antry <9antry@gmail.com>
2022-12-12 22:05:48 +0900

    add_level0_001.java

---
+++ Programmers/level0/level0_001.java
@@ -0,0 +1,14 @@
+package Programmers.level0;
+
+public class level0_001 {
+    // level0 - 두 수의 합
+}
+
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        
+        answer = num1 + num2;
+        return answer;
+    }
+}
