de8084e5bb194412460d3c91f0225150ce0e0531
9antry <9antry@gmail.com>
2022-12-16 22:53:10 +0900

    add_level0_006.java

---
+++ BAEKJOON/1. 입출력과 사칙연산/java2588.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class java2588 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int temp = 10;
+        int total = 0;
+
+        for(int i=1; i<=3; i++){
+            System.out.println(A * (B%temp));
+            B /= 10;
+            temp = temp * 10;
+            
+        }
+
+        System.out.println(A * B);
+
+        sc.close();
+    }
+}
---
+++ BAEKJOON/1. 입출력과 사칙연산/test.java
@@ -0,0 +1,42 @@
+public class test{
+    public static void main(String args[]){ 
+      
+
+           
+      
+
+        int i = 3; int k = 1;  
+    
+
+        switch(i) {  
+    
+
+          case 1: k += 1;
+    
+
+          case 2: k++;
+    
+
+          case 3: k += 0;  break; 
+    
+
+          case 4: k += 3;  break; 
+    
+
+          case 5: k -= 10;  break; 
+    
+
+          default: k--;  break; 
+    
+
+        } 
+    
+
+        System.out.print(k);  
+    
+
+         
+    
+
+      }
+}
