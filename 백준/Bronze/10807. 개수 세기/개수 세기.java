a110ff664f600aefca5cbf9e9fb44a9f2f9c2467
9antry <114674776+9antry@users.noreply.github.com>
2023-01-07 23:53:22 +0900

    [Bronze V] Title: 개수 세기, Time: 220 ms, Memory: 17828 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();// 정수의 개수(1<=N<=100)
+        int[] arr = new int[N];
+        int count = 0;
+
+        for(int i=0; i<N; i++){
+            arr[i] = sc.nextInt();
+        }
+
+        int v = sc.nextInt();
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == v)
+                count++;
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
