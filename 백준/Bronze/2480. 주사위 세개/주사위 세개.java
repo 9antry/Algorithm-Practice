75bc793e6421c6a2bc496cbdc16c5900b3c7d666
9antry <114674776+9antry@users.noreply.github.com>
2023-01-13 03:54:03 +0900

    [Bronze IV] Title: 주사위 세개, Time: 200 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+
+        sc.close(); 
+
+        int money = -1;
+
+        if(A==B&&B==C)// A B C 모두 같은 경우
+            money = 10000 + A*1000;
+        else{
+            if(A==B||A==C)// A=B 경우 or A=C 경우
+                money = 1000 + A*100;
+            else if(B==C)// B=C 경우
+                money = 1000 + B*100;
+            else{// 모두 다른 경우
+                int max = 1;
+                if(A>max) max = A;
+                if(B>max) max = B;
+                if(C>max) max = C;  
+                money = max*100;
+            }
+        }
+
+        System.out.print(money);
+    }
+}
